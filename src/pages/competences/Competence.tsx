1e495e4309c390ef28a84ec1b0c36e9d33d24554
Sheilla Silva <sheilla.silva@ccc.ufcg.edu.br>
2024-03-10 11:17:50 -0300

    feat: add competence component

---
+++ src/pages/competences/Competence.tsx
@@ -0,0 +1,30 @@
+import { FC } from "react";
+import Image from 'next/image'
+
+import { Competence as CompetenceItem } from "../../app/interfaces/competence";
+
+interface CompetenceProps  {
+  competence: CompetenceItem
+}
+
+const Competence: FC<CompetenceProps>  = ({ competence }) => {
+  const { title, description } = competence;
+
+  return (
+    <div className="competence-container">
+      <Image
+        className="competence-image"
+        src="https://i.pravatar.cc/300"
+        alt={title}
+        width={100}
+        height={100}
+      />
+      <div className="competence-context">
+        <h3 className="competence-title">{title}</h3>
+        <article>{description}</article>
+      </div>
+    </div>
+  )
+}
+
+export default Competence;
