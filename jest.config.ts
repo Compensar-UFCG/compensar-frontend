8127aea70f9ac082344750779f049ebd97c6b9c5
Sheilla Silva <sheilla.silva@ccc.ufcg.edu.br>
2024-03-10 15:45:52 -0300

    chore: add new packages and config to run unit tests

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,12 @@
 module.exports = {
+  preset: 'ts-jest',
+  verbose: true,
   testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ["@testing-library/jest-dom"],
   moduleNameMapper: {
-    '\\.(css|scss)$': 'identity-obj-proxy',
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^@app/(.*)$': '<rootDir>/src/app/$1',
+    '^@components/(.*)$': '<rootDir>/src/app/components/$1',
+    '^@styles/(.*)$': '<rootDir>/src/app/styles/$1',
   },
 };
