888d5e17c9e5d759fcb468c24899c0cbc1d432e0
Sheilla Silva <sheilla.silva@ccc.ufcg.edu.br>
2024-03-11 11:00:11 -0300

    test: add unit test to competence section in home page

---
+++ src/pages/home/index.spec.tsx
@@ -0,0 +1,45 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import competencesMock from './components/competences/tests/mocks/competencesMock.json';
+import Home, { getServerSideProps} from '@/pages/home';
+
+describe('Home', () => {
+  describe('Competences section', () => {
+    it.each(competencesMock.slice(0, 4))('render competence section list', async ({ title, description}) => {
+      global.fetch = jest.fn().mockResolvedValue({
+        json: jest.fn().mockResolvedValue(competencesMock),
+      });
+
+      const { props } = await getServerSideProps();
+      render(<Home {...props}/>);
+      
+      const titleSectionCompetences = screen.getByText('Competências')
+      const titleElement = screen.getByText(title);
+      const descriptionElement = screen.getByText(description);
+      
+      expect(titleSectionCompetences).toBeInTheDocument();
+      expect(titleElement).toBeInTheDocument();
+      expect(descriptionElement).toBeInTheDocument();
+    });
+    it('No render competence section when competence list is empty', async () => {
+      global.fetch = jest.fn().mockResolvedValue({
+        json: jest.fn().mockResolvedValue([]),
+      });
+      const { props } = await getServerSideProps();
+      render(<Home {...props}/>);
+      
+      const titleSectionCompetences = screen.queryByText('Competências');
+
+      expect(titleSectionCompetences).not.toBeInTheDocument();
+    });
+
+    it('No render competence section when API return error', async () => {
+      global.fetch = jest.fn().mockRejectedValue(new Error('Internal Server Error'));
+      const { props } = await getServerSideProps();
+      render(<Home {...props}/>);
+      
+      const titleSectionCompetences = screen.queryByText('Competências');
+
+      expect(titleSectionCompetences).not.toBeInTheDocument();
+    });
+  });
+});
