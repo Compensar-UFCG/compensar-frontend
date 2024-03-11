f279555573ac17e907604656e479aa851c606713
Sheilla Silva <sheilla.silva@ccc.ufcg.edu.br>
2024-03-10 22:41:18 -0300

    chore: adjust jest configs

--- jest.config.ts
+++ jest.config.ts
@@ -2,11 +2,14 @@ module.exports = {
   preset: 'ts-jest',
   verbose: true,
   testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ["@testing-library/jest-dom"],
+  setupFilesAfterEnv: ['@testing-library/jest-dom'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
     '^@app/(.*)$': '<rootDir>/src/app/$1',
     '^@components/(.*)$': '<rootDir>/src/app/components/$1',
     '^@styles/(.*)$': '<rootDir>/src/app/styles/$1',
   },
+  transform: {
+    '\\.[jt]sx?$': 'babel-jest',
+  }
 };
