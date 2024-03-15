15a52290419e52218f144a2675f25367bd04290b
Sheilla Silva <sheilla.silva@ccc.ufcg.edu.br>
2024-03-15 15:25:43 -0300

    feat: update account page

--- src/pages/create-account/index.tsx
+++ src/pages/create-account/index.tsx
@@ -1,16 +1,41 @@
 import './styles.scss';
+import ThemeContainer from '@/app/components/templates/ThemeContainer';
+import { useRouter } from 'next/router';
 
 import CreateAccountForm from '@components/pages/create-account/CreateAccountForm';
-import { Typography } from '@mui/material';
+import { Typography, Button } from '@mui/material';
 
 const CreateUser = () => {
+  const router = useRouter();
+
+  const redirectToLoginPage = () => {
+    router.push('/login')
+  }
   return (
-    <main className="create-user_main">
-      <Typography variant="h2" component="h2">
-        Crie sua conta
-      </Typography>
-      <CreateAccountForm/>
-    </main>
+    <ThemeContainer>
+      <main>
+        <section className="create-account_form">
+          <div className="create-account_header-section">
+            <Typography variant="h2" component="h2" align="center">
+              Crie sua conta
+            </Typography>
+            <Typography variant="subtitle2" component="h2" align="center">
+              Cadastre-se para ter acesso aos nossos conteúdos.
+            </Typography>
+          </div>
+          <CreateAccountForm/>
+        </section>
+        <section className="create-account_login">
+          <Typography variant="subtitle1" component="h2" align="center">
+            Tem uma conta?
+          </Typography>
+          <Button
+            variant="text"
+            onClick={redirectToLoginPage}
+          >Conecte-se</Button>
+        </section>
+      </main>
+    </ThemeContainer>
   );
 };
 
