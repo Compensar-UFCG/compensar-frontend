7aeb4f925f4e62f804d0f434d1982ae17f357b74
Sheilla Silva <sheilla.silva@ccc.ufcg.edu.br>
2024-03-16 15:44:57 -0300

    fix: add error messages

--- src/app/components/pages/create-account/utils/accountValidationSchema.ts
+++ src/app/components/pages/create-account/utils/accountValidationSchema.ts
@@ -1,9 +1,19 @@
 import * as yup from "yup"
 
 const accountValidationSchema = yup.object().shape({
-  username: yup.string().required().min(3).max(15).matches(/^\S*$/),
-  email: yup.string().email().required(),
-  password: yup.string().required().min(8).max(12).matches(/^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]).*$/),
+  username: yup.string()
+    .required('O nome de usuário é obrigatório.')
+    .min(3, 'O nome de usuário deve ter pelo menos 3 caracteres.')
+    .max(15, 'O nome de usuário deve ter no máximo 15 caracteres.')
+    .matches(/^\S*$/, 'O nome de usuário não pode conter espaçamento em branco.'),
+  email: yup.string()
+    .required('O email é obrigatório.')
+    .email('O email é inválido.'),
+  password: yup.string()
+    .required('A senha  é obrigatória.')
+    .min(8, 'A senha deve conter pelo menos 8 caracteres.')
+    .max(12, 'A senha deve ter no máximo 12 caracteres.')
+    .matches(/^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]).*$/, 'A senha deve conter pelo menos uma letra maiúscula, um número e um caractere especial.'),
 });
 
 export default accountValidationSchema;
