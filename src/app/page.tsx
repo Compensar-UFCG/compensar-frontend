b7f5642546f294b30940473f3d41cfb573b35dec
Sheilla Silva <sheilla.silva@ccc.ufcg.edu.br>
2024-03-19 06:05:09 -0300

    refactor: update home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,34 @@
 'use client'
-import { Button, Typography } from "@mui/material";
-import { useRouter } from 'next/navigation'
+import './styles.scss';
+
+import { Container, Typography } from "@mui/material";
+import ThemeContainer from "./components/templates/ThemeContainer";
+import Header from "./components/pages/homepage/Header";
+import Competences from "@components/pages/competences/Competences";
+import { Competences as CompetencesType } from '@app/interfaces/competence.types';
+import competencesMock from '@components/pages/competences/tests/mocks/competencesMock.json';
 
 export default function Home() {
-  const router = useRouter();
-  const redirectToCreateAccountPage = () => {
-    router.push('/create-account');
-  }
-  const redirectToLoginPage = () => {
-    router.push('/login')
-  }
   return (
-    <main>
-      <Typography variant="h1" component="h2">
-        Home Page
-      </Typography>
-      <Button
-        variant="contained"
-        onClick={redirectToCreateAccountPage}
-      >Criar conta</Button>
-      <Button
-        variant="contained"
-        onClick={redirectToLoginPage}
-      >Login</Button>
-    </main>
+    <ThemeContainer>
+      <>
+        <Header/>
+        <main>
+          <Container sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            gap: '24px',
+            margin: '24px 16px',
+          }}>
+            <Typography variant='h4' gutterBottom>Pensamento Computacional</Typography>
+            <Competences competences={competencesMock} />
+          </Container>
+        </main>
+        <footer>
+        </footer>
+      </>
+    </ThemeContainer>
   );
 }
