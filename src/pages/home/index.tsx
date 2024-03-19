2196037f2454a4a47ed86b79a89e3495191d5fe7
Sheilla Silva <sheilla.silva@ccc.ufcg.edu.br>
2024-03-19 08:21:37 -0300

    refactor: update home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,35 +1,53 @@
 import './styles.scss';
-import Competences from "@components/organisms/competences/Competences";
 import { Competences as CompetencesType } from '@app/interfaces/competence.types';
+import ThemeContainer from '@/app/components/templates/ThemeContainer';
+import { Questions } from '@/app/interfaces/question.types';
+import QuestionList from '@/app/components/pages/home';
+import Header from '@/app/components/pages/homepage/Header';
+import Competences from '@/app/components/organisms/competences/Competences';
+import { Container } from '@mui/material';
 
 interface HomeProps {
   competences: CompetencesType;
-  isEmpty: boolean
+  questions: Questions;
 }
 
-const Home = ({ competences, isEmpty }: HomeProps) => {
+const Home = ({ competences, questions }: HomeProps) => {
   return (
-    <>
-      {!isEmpty && (
-        <section className='section-competences'>
-          <h2>Competências</h2>
-          <Competences competences={competences} />
-        </section>
-      )}
-    </>
+    <ThemeContainer>
+      <>
+        <Header />
+        <main>
+          <Container sx={{ margin: '24px 16px' }}>
+            <Competences competences={competences} />
+          </Container>
+          <Container sx={{
+            display: 'grid',
+            gridTemplateColumns: '1fr 2fr',
+            gap: '16PX'
+          }}>
+            <>My list</>
+            <QuestionList questions={questions}/>
+          </Container>
+        </main>
+      </>
+    </ThemeContainer>
   );
 };
 
 export async function getServerSideProps() {
-  const competences: CompetencesType =
-    await fetch(`${process.env.BASE_API_URL}/api/competences`)
+  const competences: CompetencesType = await fetch(`${process.env.BASE_API_URL}/api/competences`)
+    .then(res => res.json())
+    .catch(() => []);
+
+  const questions: Questions =await fetch(`${process.env.BASE_API_URL}/api/questions`)
     .then(res => res.json())
     .catch(() => []);
 
   return {
     props: {
-      isEmpty: competences?.length === 0,
       competences,
+      questions
     },
   };
 }
