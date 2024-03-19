be308382baddd047619a34686ce2b893d48121ec
Sheilla Silva <sheilla.silva@ccc.ufcg.edu.br>
2024-03-19 07:42:51 -0300

    feat: use competences container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,26 @@
 'use client'
 import './styles.scss';
 
-import { Container, Typography } from "@mui/material";
 import ThemeContainer from "./components/templates/ThemeContainer";
 import Header from "./components/pages/homepage/Header";
-import Competences from "@components/pages/competences/Competences";
-import { Competences as CompetencesType } from '@app/interfaces/competence.types';
-import competencesMock from '@components/pages/competences/tests/mocks/competencesMock.json';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import CompetencesContainer from '@components/organisms/competences';
 
 export default function Home() {
+  const queryClient = new QueryClient()
+
   return (
-    <ThemeContainer>
-      <>
-        <Header/>
-        <main>
-          <Container sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            justifyContent: 'center',
-            gap: '24px',
-            margin: '24px 16px',
-          }}>
-            <Typography variant='h4' gutterBottom>Pensamento Computacional</Typography>
-            <Competences competences={competencesMock} />
-          </Container>
-        </main>
-        <footer>
-        </footer>
-      </>
-    </ThemeContainer>
+    <QueryClientProvider client={queryClient}>
+      <ThemeContainer>
+        <>
+          <Header/>
+          <main>
+            <CompetencesContainer />
+          </main>
+          <footer>
+          </footer>
+        </>
+      </ThemeContainer>
+    </QueryClientProvider>
   );
 }
