693c51778225a5a9de797e1578b86f5f9d32e81c
Sheilla Silva <sheilla.silva@ccc.ufcg.edu.br>
2024-03-18 23:37:40 -0300

    feat: add navbar

---
+++ src/app/components/organisms/NavBar.tsx
@@ -0,0 +1,45 @@
+import { FC } from "react";
+import { useRouter } from "next/navigation";
+
+import { Avatar, Box, Button, Typography } from '@mui/material';
+import { cyan } from '@mui/material/colors';
+
+const NavBar: FC = () => {
+  const router = useRouter();
+
+  const redirectToCreateAccountPage = () => {
+    router.push('/create-account');
+  }
+
+  const redirectToLoginPage = () => {
+    router.push('/home')
+  }
+
+  return (
+    <nav className="navbar">
+      <Box sx={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        gap: '6px',
+      }}>
+        <Avatar alt="logo" sx={{ bgcolor: cyan[500] }}>C</Avatar>
+        <Typography variant="subtitle1">Compensar</Typography>
+      </Box>
+      <Box>
+        <Button
+          variant="text"
+          size="small"
+          onClick={redirectToCreateAccountPage}
+        >Criar conta</Button>
+        <Button
+          variant="text"
+          size="small"
+          onClick={redirectToLoginPage}
+        >Login</Button>
+      </Box>
+    </nav>
+  )
+}
+
+export default NavBar;
