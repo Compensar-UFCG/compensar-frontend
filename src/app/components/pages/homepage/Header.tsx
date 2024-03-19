e0dc8eb5aa7060a6c67f64a6a2bcc119ae99d6bd
Sheilla Silva <sheilla.silva@ccc.ufcg.edu.br>
2024-03-19 06:04:38 -0300

    feat: create header component

---
+++ src/app/components/pages/homepage/Header.tsx
@@ -0,0 +1,34 @@
+import { FC } from "react";
+import { useRouter } from "next/navigation";
+
+import { Box, Button, Container, Typography } from "@mui/material";
+import NavBar from "@components/organisms/NavBar";
+
+const Header: FC = () => {
+  const router = useRouter();
+
+  const redirectToCreateAccountPage = () => {
+    router.push('/create-account');
+  }
+  return (
+    <header>
+      <Container sx={{
+        padding: '12px 16px 42px',
+      }}>
+        <NavBar/>
+        <Box sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}>
+          <Typography variant="subtitle1" gutterBottom>The making of a Marvel prototype</Typography>
+          <Typography variant="h1" gutterBottom>Compensar</Typography>
+          <Button variant="contained" onClick={redirectToCreateAccountPage}>CRIAR CONTA</Button>
+        </Box>
+      </Container>
+    </header>
+  )
+}
+
+export default Header;
