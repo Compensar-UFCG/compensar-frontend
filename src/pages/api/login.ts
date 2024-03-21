23c96c269eb6d9b81b93057fde6277c4375f2b2d
Sheilla Silva <sheilla.silva@ccc.ufcg.edu.br>
2024-03-21 12:34:02 -0300

    feat: add login API

---
+++ src/pages/api/login.ts
@@ -0,0 +1,21 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function users(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === 'POST') {
+    try {
+      const response = await fetch(`${process.env.BASE_API_URL}/api/login`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(req.body),
+      });
+      res.status(response.status).json(response.body)
+    } catch (error: any) {
+      res.status(error.status).json(error.body)
+    }
+  }
+}
