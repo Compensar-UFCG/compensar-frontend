92f0670e7586e1cd7469f698233b972ec48dabe6
Sheilla Silva <sheilla.silva@ccc.ufcg.edu.br>
2024-03-22 09:18:00 -0300

    refactor: remove container to competence list

--- src/app/components/organisms/competences/Competences.tsx
+++ src/app/components/organisms/competences/Competences.tsx
@@ -1,24 +1,28 @@
 import { FC } from "react";
 import { Competences as CompetenceList } from "@app/interfaces/competence.types";
 import Competence from "./Competence";
-import { Container, Grid } from "@mui/material";
+import { Grid } from "@mui/material";
 interface CompetencesProps {
   competences: CompetenceList
 }
 
 const Competences: FC<CompetencesProps>  = ({ competences }) => {
   return (
-    <Container sx={{ display: 'flex', alignItems: 'center'}}>
-      <Grid container spacing={2} sx={{
-          justifyContent: 'space-evenly',
-          alignContent: 'space-evenly',
-          alignItems: 'center',
-          gap: '8px' }}>
-        {competences.map((competence, index) => (
-          <Competence key={index} competence={competence}/>
-        ))}
-      </Grid>
-    </Container>
+    <Grid
+      container
+      spacing={2}
+      sx={{
+        justifyContent: 'space-evenly',
+        alignContent: 'space-evenly',
+        alignItems: 'center',
+        rowGap: '16px',
+        marginTop: 0
+      }}
+    >
+      {competences.map((competence, index) => (
+        <Competence key={index} competence={competence}/>
+      ))}
+    </Grid>
   )
 }
 
