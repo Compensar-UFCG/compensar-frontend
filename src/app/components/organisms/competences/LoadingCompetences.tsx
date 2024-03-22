1f9adb9c7d0374effad2528b25eca82aaeec05db
Sheilla Silva <sheilla.silva@ccc.ufcg.edu.br>
2024-03-22 09:26:18 -0300

    fix: remove container

--- src/app/components/organisms/competences/LoadingCompetences.tsx
+++ src/app/components/organisms/competences/LoadingCompetences.tsx
@@ -11,16 +11,14 @@ const LoadingCompetences = () => {
       margin: '24px 16px',
     }}>
       <Skeleton variant="rectangular" sx={{ width: '800px', height: '40px' }} />
-      <Container sx={{ display: 'flex', alignItems: 'center'}}>
-        <Grid container spacing={2} sx={{
-          justifyContent: 'space-evenly',
-          alignContent: 'space-evenly',
-          alignItems: 'center',
-          gap: '8px' }}
-        >
-          {Array.from({ length: 9 }, () => 0).map((index) => <Skeleton key={index} variant="rectangular" sx={{ width: '360px', height: '280px' }} />)}
-        </Grid>
-      </Container>
+      <Grid container spacing={2} sx={{
+        justifyContent: 'space-evenly',
+        alignContent: 'space-evenly',
+        alignItems: 'center',
+        gap: '8px' }}
+      >
+        {Array.from({ length: 9 }, () => 0).map((index) => <Skeleton key={index} variant="rectangular" sx={{ width: '360px', height: '280px' }} />)}
+      </Grid>
     </Container>
   )
 }
