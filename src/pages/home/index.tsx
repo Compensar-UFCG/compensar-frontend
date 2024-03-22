c86767678507839af5c3ec543bf8e7ffadc279d6
Sheilla Silva <sheilla.silva@ccc.ufcg.edu.br>
2024-03-22 09:43:40 -0300

    refactor: remove competence list from questions page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -5,25 +5,19 @@ import { Competences } from "@app/interfaces/competence.types";
 import { Questions } from "@app/interfaces/question.types";
 
 interface ViewProps {
-  competences: Competences;
   questions: Questions;
 }
-const View: FC<ViewProps> = ({ competences, questions }) => {
-  return <HomePage competences={competences} questions={questions} />
+const View: FC<ViewProps> = ({ questions }) => {
+  return <HomePage questions={questions} />
 }
 
 export async function getServerSideProps() {
-  const competences: Competences = await fetch(`${process.env.BASE_API_URL}/api/competences`)
-    .then(res => res.json())
-    .catch(() => []);
-
   const questions: Questions = await fetch(`${process.env.BASE_API_URL}/api/questions`)
     .then(res => res.json())
     .catch(() => []);
 
   return {
     props: {
-      competences,
       questions
     },
   };
