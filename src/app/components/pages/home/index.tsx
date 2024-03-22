f14b8afeb973482f2874a2cadb300f2b6b75b04f
Sheilla da Silva <sheilla.silva@ccc.ufcg.edu.br>
2024-03-22 09:48:34 -0300

    refactor: remove competence list from questions page (#10)

--- src/app/components/pages/home/index.tsx
+++ src/app/components/pages/home/index.tsx
@@ -3,26 +3,20 @@ import ThemeContainer from '@components/templates/ThemeContainer';
 
 import QuestionList from '@/app/components/organisms/questions';
 import Header from '@/app/components/organisms/Header';
-import Competences from '@components/organisms/competences/Competences';
 import { Container } from '@mui/material';
 
 import { Questions } from '@app/interfaces/question.types';
-import { Competences as CompetencesType } from '@app/interfaces/competence.types';
 
 interface HomePageProps {
-  competences: CompetencesType;
   questions: Questions;
 }
 
-const HomePage = ({ competences, questions }: HomePageProps) => {
+const HomePage = ({ questions }: HomePageProps) => {
   return (
     <ThemeContainer>
       <>
         <Header />
         <main>
-          <Container sx={{ margin: '24px 16px' }}>
-            <Competences competences={competences} />
-          </Container>
           <Container sx={{
             display: 'grid',
             gridTemplateColumns: '1fr 2fr',
