4d2bd4039962437731ce2d1f3baf976d9e76c2a0
Sheilla Silva <sheillasilvasp@gmail.com>
2024-03-28 07:50:10 -0300

    feat: add new functions to home provider

--- src/app/contexts/HomeProvider.tsx
+++ src/app/contexts/HomeProvider.tsx
@@ -5,20 +5,32 @@ import { Question, Questions } from '@interfaces/question.types';
 interface HomeContextProps {
   myQuestions: Questions;
   saveMyQuestions: (question: Question) => void;
+  removeMyQuestions: (question: Question) => void;
+  containQuestion: (questionId: string) => boolean;
 }
 
 const HomeContext = createContext<HomeContextProps>({
   myQuestions: [],
-  saveMyQuestions: (question: Question) => console.log(question)
+  saveMyQuestions: (question: Question) => console.log(question),
+  removeMyQuestions: (question: Question) => console.log(question),
+  containQuestion: (questionId: string) => false,
 });
 
 export const HomeProvider = ({ children }: { children: React.ReactNode }) => {
   const [myQuestions, setQuestions] = useState<Questions>([])
 
-  const saveMyQuestions = (question: Question) => setQuestions([...myQuestions, question]);
+  const saveMyQuestions = (question: Question) => {
+    setQuestions([...myQuestions, question]);
+  };
+
+  const removeMyQuestions = (question: Question) => {
+    setQuestions(myQuestions.filter(questionElement => questionElement._id !== question._id));
+  };
+
+  const containQuestion = (questionId: string) => !!myQuestions.filter(questionElement => questionElement._id === questionId).length
 
   return (
-    <HomeContext.Provider value={{ myQuestions, saveMyQuestions }}>
+    <HomeContext.Provider value={{ myQuestions, saveMyQuestions, removeMyQuestions, containQuestion }}>
       {children}
     </HomeContext.Provider>
   );
