89235c42bc96a07d5ce0e49ad0e218fd6416f670
Sheilla Silva <sheillasilvasp@gmail.com>
2024-04-04 10:01:43 -0300

    refactor: update questions list component

--- src/app/components/organisms/questions/index.tsx
+++ src/app/components/organisms/questions/index.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react";
 
-import { List, ListItem, Skeleton } from "@mui/material";
+import { Backdrop, CircularProgress, Container, List, ListItem, Skeleton } from "@mui/material";
 import Question from "@components/molecules/question/Question";
 
 import useQueryQuestions from "@hooks/useQueryQuestions";
@@ -11,19 +11,32 @@ interface QuestionListProps {
 const QuestionList: FC<QuestionListProps> = ({ showBtnAdd }) => {
   const { questions, isLoading } = useQueryQuestions();
 
-  if(isLoading) return <Skeleton variant="rectangular" sx={{ height: '90vh', margin: '16px 0' }} />
+  if(isLoading) return (
+    <Backdrop
+      sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+      open={isLoading}
+      >
+      <CircularProgress color="inherit" />
+    </Backdrop>
+  )
 
   return (
-    <List sx={{
-      display: 'flex',
-      flexDirection: 'column',
+    <Container sx={{
+      overflowY: 'scroll',
+      height: '84vh'
     }}>
-      {questions.map((question, index) =>
-        <ListItem key={index} alignItems="flex-start">
-          <Question question={question} showBtnAdd={showBtnAdd}/>
-        </ListItem>
-      )}
-    </List>
+      <List sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        
+      }}>
+        {questions.map((question, index) =>
+          <ListItem key={index} alignItems="flex-start">
+            <Question question={question} showBtnAdd={showBtnAdd}/>
+          </ListItem>
+        )}
+      </List>
+    </Container>
   )
 }
 
