6a27acdb7272e6ac6cc4793dd3cd1755eb8f1952
Sheilla Silva <sheillasilvasp@gmail.com>
2024-04-05 13:26:57 -0300

    fix: google meta tag verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
-import Head from "next/head";
 
 const roboto = Roboto({
   weight: '400',
@@ -11,6 +10,9 @@ const roboto = Roboto({
 export const metadata: Metadata = {
   title: "Compensar",
   description: "plataforma de questões avaliadas com competências do pensamento computacional",
+  verification: {
+    google: "dQHgKypQ76owi1rp8tuZ9oOuTytNcGW-VVBt4DFfOg8"
+  }
 };
 
 export default function RootLayout({
@@ -20,11 +22,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <Head>
-        <title>Compensar</title>
-        <meta property="og:title" content="plataforma de questões avaliadas com competências do pensamento computacional" key="title" />
-        <meta name="google-site-verification" content="dQHgKypQ76owi1rp8tuZ9oOuTytNcGW-VVBt4DFfOg8" />
-      </Head>
       <body className={roboto.className}>{children}</body>
     </html>
   );
